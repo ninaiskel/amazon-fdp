f943500c54347ec121059e61772e134bc2d9079f
Jessica Aiskel Aristimuño <jeakel@gmail.com>
2018-03-05 10:11:52 +0100

    add improvements

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,30 @@
-const input = document.getElementById('foo')
+const input = document.getElementById('foo');
+const buttonLess = document.getElementById('button-less');
 
 const lessButton = () => {
-	let value = Number(input.value)
-	console.log(typeof value)
-	if ( value > 1 ) {
-		value = value - 1
+	if(Number(input.value) > 1) {
+		input.value = Number(input.value) - 1;
+		buttonLess.className = 'button-form';
+	} else {
+		buttonLess.className = 'button-form disable';
 	}
 }
 
 const moreButton = () => {
-	input.value = Number(input.value) + 1
+	input.value = Number(input.value) + 1;
+	buttonLess.className = 'button-form';
+}
+
+const bigPhoto = document.getElementById('big-photo');
+const carousel = document.getElementById('carousel');
+const images = carousel.getElementsByTagName('img');
+
+const displayBigPhoto = (photo, index) => {
+	bigPhoto.src = photo
+	images[index].className = 'img-active'
+	for (var i = 0; i < images.length; i++) {
+		if ( i !== index ) {
+			images[i].className = ''
+		}
+	}
 }
